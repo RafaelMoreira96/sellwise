62b5a6c3a9b44909b90eb4830886d338dd9557d7
Rafael Moreira <rafaelmoreira.at@hotmail.com>
2024-11-01 18:54:53 -0400

    fix: refactoring dashboard html

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -3,13 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { SidebarComponent } from './components/layout/sidebar/sidebar.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ProdutoListComponent } from './pages/produto/produto-list/produto-list.component';
-import { ProdutoFormComponent } from './pages/produto/produto-form/produto-form.component';
 import { ClienteListComponent } from './pages/cliente/cliente-list/cliente-list.component';
-import { ClienteFormComponent } from './pages/cliente/cliente-form/cliente-form.component';
 import { FornecedorListComponent } from './pages/fornecedor/fornecedor-list/fornecedor-list.component';
-import { FornecedorFormComponent } from './pages/fornecedor/fornecedor-form/fornecedor-form.component';
 import { FuncionarioListComponent } from './pages/funcionario/funcionario-list/funcionario-list.component';
-import { FuncionarioFormComponent } from './pages/funcionario/funcionario-form/funcionario-form.component';
 import { FormaPagamentoListComponent } from './pages/forma-pagamento/forma-pagamento-list/forma-pagamento-list.component';
 import { FormaPagamentoFormComponent } from './pages/forma-pagamento/forma-pagamento-form/forma-pagamento-form.component';
 import { ConfigPageComponent } from './pages/configuration/config-page/config-page.component';
@@ -29,18 +25,12 @@ const routes: Routes = [
 
       // Customers routes
       { path: 'customers', component: ClienteListComponent },
-      { path: 'add-customer', component: ClienteFormComponent },
-      { path: 'edit-customer/:id', component: ClienteFormComponent },
 
       // Suppliers routes
       { path: 'suppliers', component: FornecedorListComponent },
-      { path: 'add-supplier', component: FornecedorFormComponent },
-      { path: 'edit-supplier/:id', component: FornecedorFormComponent },
 
       // Employee routes
       { path: 'employees', component: FuncionarioListComponent },
-      { path: 'add-employee', component: FuncionarioFormComponent },
-      { path: 'edit-employee/:id', component: FuncionarioFormComponent },
 
       // Payment methods routes
       { path: 'payment-methods', component: FormaPagamentoListComponent },
